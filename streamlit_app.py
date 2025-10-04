368a746e35715b75fb395f8189e4d5516c7ba511
jmonit <monitunleashed@gmail.com>
2025-10-04 09:19:43 +0000

    Tryung a change

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 @st.cache_data
 def get_gdp_data():
-    """Grab GDP data from a CSV file.
+    """Grab GDP data from a CSV file. Nice
 
     This uses caching to avoid having to read the file every time. If we were
     reading from an HTTP endpoint instead of a file, it's a good idea to set
@@ -26,7 +26,7 @@ def get_gdp_data():
     raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
     MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    MAX_YEAR = 2025
 
     # The data above has columns like:
     # - Country Name
